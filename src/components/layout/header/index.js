ce13c423526b1743c0ee334f7994bf63f58ac058
Martin Andersen <marander@protonmail.com>
2021-08-17 02:36:32 +0200

    Initialize with-no-js-runtime

---
+++ src/components/layout/header/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+import { useAuth } from 'components/auth-context';
+import { useSettings } from 'components/settings-context';
+import { useT } from 'lib/i18n';
+
+import BurgerButton from './burger-button';
+import BasketButton from './basket-button';
+import LocaleSwitcher from './locale-switcher';
+import Search from './search';
+import {
+  Outer,
+  Nav,
+  Logo,
+  NavActions,
+  NavList,
+  NavListItem,
+  PreviewBar,
+  IconBar
+} from './styles';
+
+export default function Header({ simple, preview }) {
+  const t = useT();
+  const { mainNavigation } = useSettings();
+  const auth = useAuth();
+  const router = useRouter();
+
+  return (
+    <>
+      {preview && (
+        <PreviewBar>
+          You are in preview mode (
+          <a href={'/api/preview?leave=' + encodeURIComponent(router.asPath)}>
+            leave
+          </a>
+          )
+        </PreviewBar>
+      )}
+      <Outer simple={simple}>
+        <Link href="/" passHref>
+          <Logo>
+            <img src="/static/shop-logo.svg" alt="" width="56" height="84" />
+          </Logo>
+        </Link>
+        <Nav id="main-menu">
+          <NavList>
+            {mainNavigation?.map((category) => (
+              <NavListItem key={category.path}>
+                <Link href={category.path}>
+                  <a>{category.name}</a>
+                </Link>
+              </NavListItem>
+            ))}
+          </NavList>
+        </Nav>
+        <NavActions id="main-menu-actions">
+          <LocaleSwitcher />
+          {auth.isLoggedIn ? (
+            <button type="button" onClick={auth.logout}>
+              Logout
+            </button>
+          ) : (
+            <Link href="/login">
+              <a>{t('customer.login.title')}</a>
+            </Link>
+          )}
+        </NavActions>
+        {!simple && (
+          <IconBar>
+            <Search />
+            <BasketButton />
+          </IconBar>
+        )}
+        <BurgerButton />
+      </Outer>
+    </>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import Cmp, { getData } from 'page-components/frontpage';
+import { getLocaleFromContext } from 'lib/app-config';
+
+export async function getStaticProps(context) {
+  const { preview } = context;
+  const locale = getLocaleFromContext(context);
+
+  const data = await getData({
+    asPath: '/',
+    language: locale.crystallizeCatalogueLanguage,
+    preview
+  });
+
+  return {
+    props: data,
+    revalidate: 1
+  };
+}
+
+export default Cmp;
+
+export const config = {
+  unstable_runtimeJS: false
+};
